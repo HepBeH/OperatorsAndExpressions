d47d465a769b5a7c6585ed8d67bec05e0980d187
Vesko Vasilev <nervenkick@gmail.com>
2015-01-24 14:36:43 +0200

    OperatorsAndExpressions

---
+++ ConsoleApplication2/OddOrEvenIntegers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//Problem 1. Odd or Even Integers
+//Write an expression that checks if given integer is odd or even.
+class OddOrEvenIntegers
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter an integer:");
+        int input = Convert.ToInt32(Console.ReadLine());
+        if (input % 2 == 0)
+        {
+            Console.WriteLine(input + " is even.");
+        }
+        else
+        {
+            Console.WriteLine(input + " is odd.");
+        }
+    }
+}
